cb376a315146cc98a2a4c8c624bd1a71bfb9f8e7
joaovictor001 <joaocarvalhoborges@outlook.com>
2024-09-24 12:43:58 -0300

    :bug: fix: Arrumando erro de salvar trackinhistoric e o nome do equipamento

--- BackEnd/src/main/java/com/MapView/BackEnd/serviceImp/EquipmentServiceImp.java
+++ BackEnd/src/main/java/com/MapView/BackEnd/serviceImp/EquipmentServiceImp.java
@@ -99,29 +99,14 @@ public class EquipmentServiceImp implements EquipmentService {
             throw new OperativeFalseException("The inactive mainowner cannot be accessed.");
         }
 
-        System.out.println(data.name_equipment());
+        Equipment equipment = equipmentRepository.save(new Equipment(data, location, mainOwner));;
 
-        // Cria o equipamento
-        Equipment equipment = new Equipment(data, location, mainOwner);
-
-        // Usa merge em vez de save
-        equipment = entityManager.merge(equipment);
-        //equipmentRepository.save(equipment);
+        TrackingHistory trackingHistory = new TrackingHistory(equipment, location.getEnvironment(), equipment.getRfid(), EnumTrackingAction.ENTER, EnumColors.GREEN);
+        trackingHistoryRepository.save(trackingHistory);
 
         var userLog = new UserLog(users, "Equipment", data.id_equipment(), "Create new Equipment", EnumAction.CREATE);
         userLogRepository.save(userLog);
 
-        // Salvar o tracking history
-        Enviroment enviroment = location.getEnvironment();
-
-        TrackingHistory trackingHistory = new TrackingHistory(
-                equipment, enviroment, equipment.getRfid(), EnumTrackingAction.ENTER,
-                EnumColors.GREEN
-        );
-
-        trackingHistoryRepository.save(trackingHistory);
-
-        System.out.println(new EquipmentDetailsDTO(equipment));
         return new EquipmentDetailsDTO(equipment);
     }
 
